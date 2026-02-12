5efb95cb289d8614b35caeb2caa0d3b66220818b
Miguel Siqueira <miguel@email.com>
2026-02-12 15:57:23 -0300

    Mensagem pessoal no Next.js

---
+++ meu-projeto-next/app/page.tsx
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontSize: "28px",
+        textAlign: "center",
+        fontFamily: "Arial, sans-serif",
+        padding: "20px",
+      }}
+    >
+      Think different.
+    </main>
+  );
+}
